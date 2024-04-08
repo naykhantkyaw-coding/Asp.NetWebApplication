56b11b21da67c36bdd9435f88e76904bf0f934bb
naykhantkyaw-coding <naykhantkyaw.coding@gmail.com>
2024-04-08 23:02:06 +0630

    [Inprogress] Calling Data

--- CheckDatabaseAspDotNetWeb/Controller/GetData/DA_GetDataController.cs
+++ CheckDatabaseAspDotNetWeb/Controller/GetData/DA_GetDataController.cs
@@ -2,6 +2,7 @@
 using CheckDatabaseAspDotNetWeb.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -19,12 +20,29 @@ namespace CheckDatabaseAspDotNetWeb.Controller.GetData
                 string serverName = getServerName.GetServerName();
                 if(serverName != null && reqModel.DbName != null)
                 {
-                    connectionString = $""
+                    connectionString = $"Data Source = {serverName};Initial Catalog ={reqModel.DbName};Integrated Security = true;";
+                }
+                using(SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    if(reqModel.TableName != null)
+                    {
+                        using(SqlCommand cmd = new SqlCommand($"select * fromt {reqModel.TableName}", con))
+                        {
+                            using(SqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+
+                                }
+                            }
+                        }
+                    }
                 }
             }
             catch (Exception ex)
             {
-
+                
                 model.Response = ex.GetResposeError();
             }
 
